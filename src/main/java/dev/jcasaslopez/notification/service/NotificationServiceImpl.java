5249b7c83e373eaa6f0f6550148af0622f5b0666
Jorge Casas <j.casas.lopez.26@gmail.com>
2026-04-10 18:19:15 +0200

    fix (service): add missing @Service annotation

--- src/main/java/dev/jcasaslopez/notification/service/NotificationServiceImpl.java
+++ src/main/java/dev/jcasaslopez/notification/service/NotificationServiceImpl.java
@@ -4,11 +4,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
 
 import dev.jcasaslopez.notification.model.Email;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 
+@Service
 public class NotificationServiceImpl implements NotificationService {
 
 	private static final Logger logger = LoggerFactory.getLogger(NotificationServiceImpl.class);
